838e55ddfa32e137aaacf50a1f2d6b075a4a0e1e
supermomonga <hi@supermomonga.com>
2015-08-27 07:41:52 +0900

    make it envs

--- app/app.el
+++ app/app.el
@@ -13,5 +13,5 @@
   (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
   (elnode-http-return httpcon (format "<html><h1>Hello Elnode</h1><p>%s</p></html>" (emacs-version))))
 
-(elnode-start 'my-test-handler :port (or (getenv "PORT") 80) :host "localhost")
+(elnode-start 'my-test-handler :port (or (getenv "ELNODE_PORT") 80) :host (or (getenv "ELNODE_HOST") "localhost"))
 
