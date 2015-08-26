181c1bbe513dc45c6979e0bad2d8975ed055d56c
supermomonga <hi@supermomonga.com>
2015-08-27 08:48:40 +0900

    make it works

--- app/app.el
+++ app/app.el
@@ -13,5 +13,7 @@
   (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
   (elnode-http-return httpcon (format "<html><h1>Hello Elnode</h1><p>%s</p></html>" (emacs-version))))
 
-(elnode-start 'my-test-handler :port (or (getenv "ELNODE_PORT") 80) :host (or (getenv "ELNODE_HOST") "localhost"))
+(elnode-start 'my-test-handler :port (or (getenv "ELNODE_PORT") 80) :host (or (getenv "ELNODE_HOST") "0.0.0.0"))
+
+(message "server started.")
 
